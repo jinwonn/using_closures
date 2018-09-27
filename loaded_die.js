b8f0c469061d4ea865ee2a1767a30bcfe1a0648c
jinwonn <d.t.nguyen@live.com>
2018-09-27 18:48:22 +0000

    read question wrong. ficed to answer question

--- loaded_die.js
+++ loaded_die.js
@@ -1,9 +1,13 @@
 function makeLoadedDie() {
   var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
   /* your code here */
+  var indexlocation = 0;
 
   return function() {
     /* your code here */
+    dicerolled = list[indexlocation];
+    indexlocation += 1;
+    return dicerolled;
   }
 }
 
@@ -12,3 +16,12 @@ var rollLoadedDie = makeLoadedDie();
 console.log(rollLoadedDie());  // 5
 console.log(rollLoadedDie());  // 4
 console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());  // 1
+console.log(rollLoadedDie());  // 6
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 2
+console.log(rollLoadedDie());  // 3
+console.log(rollLoadedDie());  // 3
+console.log(rollLoadedDie());  // 5
+
+// console.log(x)
